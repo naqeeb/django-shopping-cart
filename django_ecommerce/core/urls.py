80997df928fcaa783e08d18247e9da8fad7838fa
Naqeeb Memon <naqeeb@Naqeeb-Mac.local>
2014-02-27 19:13:03 -0500

    - Fixed add to cart bugs for logged in and anonymous users - Separated add to cart and view cart functionality

--- django_ecommerce/core/urls.py
+++ django_ecommerce/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
 	url(r'^product/(?P<product_sku>[a-zA-Z0-9-]+)/$', views.product_detail, name='product_detail'),
 
     url(r'^cart/add/$', views.add_product_to_cart, name='add_product_to_cart'),
+    url(r'^cart/(?P<cart_id>[0-9]+)/$', views.view_cart, name='view_cart'),
 
     url(r'', views.home, name='home'),
 )
--- django_ecommerce/core/views.py
+++ django_ecommerce/core/views.py
@@ -1,8 +1,9 @@
 from django.contrib.sites.models import get_current_site
-from django.shortcuts import redirect, render_to_response
+from django.db.models import F
+from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from .models import Store, Product, StoreProduct
+from .models import Store, Product, StoreProduct, Cart, CartItem
 from .forms import AddProductToCartForm
 
 def product_detail(request, product_sku, template_name='core/product.html'):
@@ -36,17 +37,34 @@ def product_detail(request, product_sku, template_name='core/product.html'):
     return render_to_response(template_name, response, context_instance=RequestContext(request))
 
 
-def add_product_to_cart(request, template_name='core/cart.html'):
+def view_cart(request, cart_id, template_name='core/cart.html'):
     response = {
         'store': None,
-        'cart': None
+        'cart': None,
+        'cart_items': None
     }
 
     # Get the current site
     site = get_current_site(request)
 
     # Get the current store
-    store = Store.objects.filter(site=site)
+    store = Store.objects.get(site=site)
+
+    response['store'] = store
+    cart = Cart.objects.get(pk=cart_id)
+    response['cart'] = cart
+    response['cart_items'] = CartItem.objects.filter(cart=cart)
+
+    return render_to_response(template_name, response, context_instance=RequestContext(request))
+
+def add_product_to_cart(request):
+    cart = None
+
+    # Get the current site
+    site = get_current_site(request)
+
+    # Get the current store
+    store = Store.objects.get(site=site)
 
     form = AddProductToCartForm(request.POST or None)
 
@@ -54,46 +72,65 @@ def add_product_to_cart(request, template_name='core/cart.html'):
         # Use the user or session to find the cart
         user_authenticated = request.user.is_authenticated()
         sku = form.cleaned_data['sku']
-        quantity = form.cleaned_data['quantity']
+        quantity = long(form.cleaned_data['quantity'])
+        product = get_object_or_404(Product, sku=sku)
 
         # Get the cart
-        cart = None
-        product = None
+        cart_item = None
         if user_authenticated:
             try:
                 cart = Cart.objects.get(user=request.user)
-                cart_items = CartItems.objects.filter(cart=cart)
-                products = [cart_item.product for cart_item in cart_items if cart_item.product__sku == sku]
-                if products:
-                    product = products[0]
-            except:
+                cart_items = CartItem.objects.filter(cart=cart)
+
+                for item in cart_items:
+                    if item.product.sku == sku:
+                        cart_item = item
+                        break
+            except Cart.DoesNotExist:
                 cart = Cart.objects.create(store=store, user=request.user)
+            except Exception as e:
+                print e
+
         else:
             session_key = request.session.session_key
             try:
                 cart = Cart.objects.get(session=session_key)
-                cart_items = CartItems.objects.filter(cart=cart)
-                products = [cart_item.product for cart_item in cart_items if cart_item.product__sku == sku]
-                if products:
-                    product = products[0]
-            except:
-                cart = Cart.objects.create(store=store,session=session_key)
+                cart_items = CartItem.objects.filter(cart=cart)
 
-        #
-        if not product:
+                for item in cart_items:
+                    if item.product.sku == sku:
+                        cart_item = item
+                        break
+            except Cart.DoesNotExist:
+                cart = Cart.objects.create(store=store, session=session_key)
+            except Exception as e:
+                print e
+
+        # Check the cart items to see if the product was added previously
+        if not cart_item:
             # Create Cart Item
-            pass
+            cart_item = CartItem(cart=cart, product=product, quantity=quantity)
+            cart_item.save()
         else:
             # Increment the Item
-            pass
+            cart_item.quantity = F('quantity') + quantity
+            cart_item.save()
+
+        return redirect('view_cart', cart_id=cart.pk)
+
+    return redirect('home')
 
 
-        # Check the cart items to see if the product was add to the cart previously
+def prepare_checkout(request, template_name='core/checkout.html'):
+    pass
+    # Find the cart
 
-        # Add or increment the item
+    # Check out only for logged in users
 
 
-    return render_to_response(template_name, response, context_instance=RequestContext(request))
+def checkout(request, template_name='core/checkout.html'):
+    pass
+    # Convert cart to order
 
 
 def home(request, template_name='core/store.html'):
