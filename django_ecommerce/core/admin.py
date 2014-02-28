74f6bdf0096cedef09465403dcf6fa3d972f5aec
Naqeeb Memon <naqeeb@Naqeeb-Mac.local>
2014-02-28 15:09:10 -0500

    Added Orders and Carts to admin

--- django_ecommerce/core/admin.py
+++ django_ecommerce/core/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
-from core.models import Store
+from core.models import Store, Order, OrderItem
+from cart.models import Cart, CartItem
 from product.models import Product, StoreProduct, ProductAttribute, ProductAttributeValue
 from profile.models import UserProfile, UserStoreProfile
 
@@ -53,6 +54,41 @@ class ProductAttributeAdmin(admin.ModelAdmin):
         return qs.none()
 
 admin.site.register(ProductAttribute, ProductAttributeAdmin)
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('store', 'user', 'total')
+    search_fields = ('store__name', 'user__email')
+    inlines= [CartItemInline]
+
+admin.site.register(Cart, CartAdmin)
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'total')
+    search_fields = ('product__name', 'product__description')
+
+admin.site.register(CartItem, CartItemAdmin)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('external_id', 'store', 'user', 'total')
+    search_fields = ('external_id', 'store__name', 'user__email')
+    inlines= [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'price')
+    search_fields = ('order__external_id', 'product__name', 'product__sku')
+
+admin.site.register(OrderItem, OrderItemAdmin)
 
 class StoreProductAdmin(admin.ModelAdmin):
     list_display = ('product', 'store', 'active')
