944bc4198c1d399ce51b2cd484974f8c30766b2f
Naqeeb Memon <naqeeb@Naqeeb-Mac.local>
2014-02-28 13:15:50 -0500

    Added restrictions to admin

--- django_ecommerce/core/admin.py
+++ django_ecommerce/core/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
-
 from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+
 from core.models import Store
 from product.models import Product, StoreProduct, ProductAttribute, ProductAttributeValue
+from profile.models import UserProfile, UserStoreProfile
 
-from django.utils.translation import ugettext_lazy as _
+from .utils import limit_qs_by_user, get_products_by_user
+
 
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('name', 'site')
     search_fields = ('name', 'site')
 
+    def queryset(self, request):
+        qs = super(StoreAdmin, self).queryset(request)
+        return limit_qs_by_user(request.user, qs)
+
 admin.site.register(Store, StoreAdmin)
+
+class UserStoreProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_profile','store')
+
+admin.site.register(UserStoreProfile, UserStoreProfileAdmin)
+
 
 class StoreProductInline(admin.TabularInline):
     model = StoreProduct
@@ -25,11 +38,19 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     inlines= [ProductAttributeValueInline, StoreProductInline]
 
+
 admin.site.register(Product, ProductAdmin)
 
 class ProductAttributeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+    def queryset(self, request):
+        qs = super(ProductAttributeAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+
+        return qs.none()
 
 admin.site.register(ProductAttribute, ProductAttributeAdmin)
 
@@ -37,5 +58,9 @@ class StoreProductAdmin(admin.ModelAdmin):
     list_display = ('product', 'store', 'active')
     search_fields = ('product__name',)
     list_filter = ('store__name', 'active')
+
+    def queryset(self, request):
+        qs = super(StoreProductAdmin, self).queryset(request)
+        return limit_qs_by_user(request.user, qs)
 
 admin.site.register(StoreProduct, StoreProductAdmin)
