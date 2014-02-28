e38d5dc0c2054d62240def2376af8184bfa1443c
Naqeeb Memon <naqeeb@Naqeeb-Mac.local>
2014-02-27 23:36:56 -0500

    Updated models database names to remove the django app prefix.

--- django_ecommerce/core/models.py
+++ django_ecommerce/core/models.py
@@ -9,6 +9,10 @@ class Store(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
+    class Meta:
+        db_table = 'store'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -19,10 +23,17 @@ class Product(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
+    class Meta:
+        db_table = 'product'
+
+
 class StoreProduct(models.Model):
     store = models.ForeignKey(Store)
     product = models.ForeignKey(Product)
     active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'store_product'
 
 class ProductAttribute(models.Model):
     name = models.CharField(max_length=255)
@@ -30,29 +41,49 @@ class ProductAttribute(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
+    class Meta:
+        db_table = 'product_attribute'
+
 class ProductAttributeValue(models.Model):
     product = models.ForeignKey(Product)
     attribute = models.ForeignKey(ProductAttribute)
     value = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'product_attribute_value'
 
 class Cart(models.Model):
     store = models.ForeignKey(Store)
     user = models.ForeignKey(User, null=True, blank=True)
     session = models.CharField(max_length=255, null=True, blank=True)
 
+    class Meta:
+        db_table = 'cart'
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart)
     product = models.ForeignKey(Product)
     quantity = models.IntegerField()
 
+    class Meta:
+        db_table = 'cart_item'
+
 class Order(models.Model):
     store = models.ForeignKey(Store)
     user = models.ForeignKey(User)
+    external_id = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     total = models.DecimalField(max_digits=9, decimal_places=2)
+
+    class Meta:
+        db_table = 'order'
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order)
     product = models.ForeignKey(Product)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_item'
+
