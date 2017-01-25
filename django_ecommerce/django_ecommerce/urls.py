e08a19f0553d44d39e83157159b5d0558778942f
Naqeeb Memon <naqeeb.memon@gmail.com>
2017-01-24 23:43:42 -0600

    enabled the /orders endpoint to retrieve orders for the current user

--- django_ecommerce/django_ecommerce/urls.py
+++ django_ecommerce/django_ecommerce/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
+from rest_framework import routers
 
 from django.contrib import admin
 admin.autodiscover()
 
-from core import views
+from core import views, viewsets
+
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'orders', viewsets.OrderViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -11,5 +15,7 @@ urlpatterns = [
     url(r'^product/', include('product.urls')),
     url(r'^carts/', include('cart.urls')),
     url(r'^checkout/', include('checkout.urls')),
-    url(r'', views.home, name='home'),
+    url(r'^home/', views.home, name='home'),
 ]
+
+urlpatterns += router.urls
