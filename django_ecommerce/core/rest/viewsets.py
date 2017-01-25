d7331d683d57f0d55acc7bdd715c1effb295b38d
Naqeeb Memon <naqeeb.memon@gmail.com>
2017-01-25 16:33:02 -0600

    added the ability to support multiple serializers depending on the REST operation

--- django_ecommerce/core/rest/viewsets.py
+++ django_ecommerce/core/rest/viewsets.py
@@ -1,6 +1,14 @@
 from rest_framework import viewsets
 
 class BaseViewSet(viewsets.ViewSet):
+    serializers = {
+        'default': None
+    }
+
+    def get_serializer_class(self):
+        """ return the correct serialzer per action """
+        return self.serializers.get(self.action,
+                        self.serializers['default'])
 
     def get_queryset(self):
         """ filter qs for current user """
