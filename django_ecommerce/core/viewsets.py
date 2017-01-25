587d09a58ecec88c913cb5be31ab7329cca2cb39
Naqeeb Memon <naqeeb.memon@gmail.com>
2017-01-25 16:34:22 -0600

    updated viewset to use multiple serializers and new serializer convention

--- django_ecommerce/core/viewsets.py
+++ django_ecommerce/core/viewsets.py
@@ -3,8 +3,12 @@ from rest_framework import viewsets
 from core.rest.viewsets import BaseViewSet
 
 from .models import Order
-from .serializers import OrderSerializer
+from .serializers import OrderListSerializer
 
 class OrderViewSet(BaseViewSet, viewsets.ReadOnlyModelViewSet):
+    serializers = {
+        'list':    OrderListSerializer,
+        'default':  OrderListSerializer
+    }
+
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
